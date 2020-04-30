c5b1a22010772f51bf7ea2f14e3647c922f5d39f
Schrius <schriusty@gmail.com>
2020-04-30 19:49:01 -0400

    factory

---
+++ factory/factory.go
@@ -0,0 +1,131 @@
+package factory
+
+type (
+	mongoDB struct {
+		database map[string]string
+	}
+
+	sqlite struct {
+		database map[string]string
+	}
+
+	file struct {
+		name string
+		content string
+	}
+
+	ntfs struct {
+		files map[string]file
+	}
+
+	ext4 struct {
+		files map[string]file
+	}
+
+	FileSystem interface {
+		CreateFile(string)
+		FindFile(string) file
+	}
+
+	Database interface {
+		GetData(string) string
+		PutData(string, string)
+	}
+
+	Factory func(string) interface{}
+)
+
+func (mdb mongoDB) GetData(query string) string {
+	if _, ok := mdb.database[query]; !ok {
+		return ""
+	}
+
+	fmt.Println("MongoDB")
+	return mdb.database[query]
+}
+
+func (sql sqlite) getData(query string) string {
+	if _, ok := sql.database[query]; !ok {
+		return ""
+	}
+
+	fmt.Println("Sqlite")
+	return sql.database[query]
+}
+
+func (mdb mongoDB) PutData(query string, data string) {
+	mdb.database[query] = data
+}
+
+func (sql sqlite) PutData(query string, data string) {
+	sql.database[query] = data
+}
+
+func (ntfs ntfs) CreateFile(path string) {
+	file := file{content: "NTFS file", name: path}
+	ntfs.file[path] = file
+	fmt.Println("NTFS")
+}
+
+func (ext4 ext4) CreateFile(path string) {
+	file := file{content: "EXT4 file", name: path}
+	ext4.files[path] = file
+	fmt.Println("EXT4")
+}
+
+func (ntfs ntfs) FindFile(path string) file {
+	if _, ok := ntfs.files[path]; !ok {
+		return file{}
+	}
+
+	return ntfs.files[path]
+}
+
+func (ext4 ext4) FindFile(path string) file {
+	if _, ok := ext4.files[path]; !ok {
+		return file{}
+	}
+
+	return ext4.files[path]
+}
+
+func FilesystemmFactory(env string) interface{} {
+	switch env {
+	case "production":
+		return ntfs{
+			files: make(map[string]file),
+		}
+	case "development":
+		return ext4{
+			files: make(map[string]file),
+		}
+	default:
+		return nil
+	}
+}
+
+func DatabaseFactory(env string) interface{} {
+	switch env {
+	case "production":
+		return mongoDB{
+			database: make(map[string]string),
+		}
+	case "development":
+		return sqlite{
+			database: make(map[string]string),
+		}
+	default:
+		return nil
+	}
+}
+
+func AbstractFactory(fact string) Factory {
+	switch fact {
+	case "database":
+		return DatabaseFactory
+	case "filesystemm":
+		return FilesystemmFactory
+	default:
+		return nil
+	}
+}
